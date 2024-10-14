e299dac568611971c9d62e01ae4e67e2c2458a3e
Roman Koshchei <romankoshchei@gmail.com>
2024-10-14 18:27:02 +0300

    non aot

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Building the project..."
-
-dotnet publish ./src/Api/Api.csproj \
-  -c Release -r win-x64 -o out \
-  -p:DebugSymbols=false -p:DebugType=none \
--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-cp ./.env ./src/Api/.env
+# cp ./.env ./src/Api/.env
 # cp ./providers.json ./src/Api/providers.json
 
-dotnet run --project ./src/Api/Api.csproj
+dotnet run --project ./EmailSwitch.csproj
 
 # rm ./src/Api/providers.json
