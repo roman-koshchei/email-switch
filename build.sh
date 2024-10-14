9ce66df9d6d9fcf5f2b611cd5f7aab8cc1b38b6b
Roman Koshchei <romankoshchei@gmail.com>
2024-10-14 10:55:11 +0300

    sync

--- build.sh
+++ build.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-PROJECT="./src/Api/Api.csproj"
-
-echo "Restoring NuGet packages..."
-dotnet restore $PROJECT
-
 echo "Building the project..."
-dotnet publish $PROJECT -c Release -r win-x64 -o bin
+
+dotnet publish ./src/Api/Api.csproj \
+  -c Release -r win-x64 -o out \
+  -p:DebugSymbols=false -p:DebugType=none \
